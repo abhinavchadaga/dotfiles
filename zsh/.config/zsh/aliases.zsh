8263e4a9e5e29314750409e77ed7a672856429ca
abhinavchadaga <abhinav.chadaga@icloud.com>
2025-07-07 23:38:41 -0500

    add lla alias

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -3,7 +3,8 @@
 # ls aliases
 alias ls="eza -1 --color=always"
 alias la="ls --all"
-alias ll="ls --long --header --git"
+alias ll="ls --long --header --git --icons"
+alias lla="ll --all"
 alias lt="ls --tree --level=2 --git-ignore"
 
 # make reloading zsh config easier
